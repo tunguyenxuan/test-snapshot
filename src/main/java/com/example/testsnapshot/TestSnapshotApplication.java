8f3ba441eded72e3b69840cb748b2649ee50dbc8
TuNguyen <ituet1119@gmail.com>
2022-06-02 01:06:05 +0700

    pull dependence from nexus

---
+++ src/main/java/com/example/testsnapshot/TestSnapshotApplication.java
@@ -0,0 +1,13 @@
+package com.example.testsnapshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSnapshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSnapshotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testsnapshot/TestSnapshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsnapshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSnapshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
